9c81a2f57a2105bc987f249d7c62dc2fa5e76097
Kelly <kelly.bondanza@gmail.com>
2016-11-19 21:24:46 +0000

    started users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,22 @@
 class UsersController < ApplicationController
+    def login
+        @user = User.find_by(email: params[:email])
+    end
+    def logout
+    end
+    def show
+    end
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            render status: :created
+        else
+            render json: {message
+        end    
+    end
+    def update
+        
+    end
+    def destroy
+    end  
 end
