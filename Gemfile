c8aa208eb58b1549b7c065e7148c950c42f1e872
a33eyes <ariiigu@gmail.com>
2016-11-20 03:48:44 +0000

    Rolled back some earier user signup stuff, installed Devise instead.

--- Gemfile
+++ Gemfile
@@ -46,3 +46,6 @@ group :development do
   gem 'spring'
 end
 
+
+# Use Devise for authentication
+gem 'devise'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   root 'users#signup'
   
   post "users", to: "users#create"
